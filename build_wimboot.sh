6fb1f03174e623a81929bd8022cb880b3a67aeb9
MexIT <2500356-MexxIT@users.noreply.gitlab.com>
2026-06-11 19:08:34 +0000

    wimboot v2.9.0

--- build_wimboot.sh
+++ build_wimboot.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 
 # Set variables
-wimboot_tag="v2.8.0"
+wimboot_tag="v2.9.0"
 
 # Download wimboot source
 git clone --single-branch https://github.com/ipxe/wimboot.git && cd wimboot
 git checkout ${wimboot_tag}
 
 git apply ../0001_Add-EFI-LoadFile2-and-InitrdMedia-headers.patch
-git apply ../0002_Fix-optinal-header-in-wimboot-2.8.0.patch
+git apply ../0002_Fix-optinal-header-in-wimboot.patch
 git apply ../0003_Provide-common-vdisk_read_mem_file-cpio-handler.patch
 git apply ../0004_Support-EFI-linux-initrd-media-loading.patch
 
